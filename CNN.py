589459d8719335e1fe7e5023f78cf7aa9eda9cf9
jjb4 <jjb043@bucknell.edu>
2025-04-29 11:54:34 +0200

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -14,3 +14,21 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import classification_report, confusion_matrix
 
+
+data_dir = 'data' # Data File
+
+categories  = ['cloudy', 'desert', 'green_area', 'water']
+data = []
+data_labels = []
+
+# Loop through each category and collect image paths
+for categorie in categories :
+    folder_path = os.path.join(data_dir, categorie)
+    image_paths = os.listdir(folder_path)
+    data.extend([os.path.join(folder_path, image_path) for image_path in image_paths])
+    data_labels.extend([categorie] * len(image_paths))
+
+# Create a DataFrame with the image paths and labels
+data = pd.DataFrame({'Image_Path': data, 'labels': data_labels})
+print(data) # Display the DataFrame
+
